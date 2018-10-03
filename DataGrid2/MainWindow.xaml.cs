dc0d5d6a13118b0849af2adda646808177ac0241
Mihkel Tiganik <mtiganik@gmail.com>
2018-10-03 11:19:25 +0300

    Add project files.

---
+++ DataGrid2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DataGrid2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGrid2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ShiftClass> Shifts { get; set; } = new List<ShiftClass>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Shifts.Add(new ShiftClass { Shift = "III.", Products = 500, FontSize = 14 });
+            Shifts.Add(new ShiftClass { Shift = "II.", Products = 1200, FontSize = 14 });
+            Shifts.Add(new ShiftClass { Shift = "I.", Products = 1000, FontSize = 20 });
+
+            DataContext = this;
+        }
+    }
+}
---
+++ DataGrid2/Models/ShiftClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid2.Models
+{
+    public class ShiftClass
+    {
+        public string Shift { get; set; }
+        public int Products { get; set; }
+        public double FontSize { get; set; }
+
+        
+    }
+}
